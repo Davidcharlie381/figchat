17e9261b484149398880f485b4ff514b38fd0c87
davidcharlie381 <davidcharleswisdom@gmail.com>
2023-12-25 17:30:40 +0100

    fix: correct heading

--- app/feed/search/page.jsx
+++ app/feed/search/page.jsx
@@ -6,7 +6,7 @@ export default function Search() {
   return (
     <>
       <main className="mx-auto max-w-xl px-4">
-        <h1 className="text-4xl font-normal pt-10 font-comfortaa">Register</h1>
+        <h1 className="text-4xl font-normal pt-10 font-comfortaa">Search</h1>
         <Input
           type="text"
           className="mt-8"
